5bbdf91726bb389ec5bc83378fd779e10fa538cf
(Student  C00306813) Conor Minihn <C00306813@setu.ie>
2024-11-08 10:41:19 +0000

    Tweaks

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -23,7 +23,7 @@ int main()
 		std::cout << "You need to select a number between 1 and 5" << std::endl;
 		return 0;
 	}
-	std::cout << "Pick a letter from A to B or M to O or Z to Z" << std::endl; // change this line each iteration
+	std::cout << "Pick a letter from A to B or L to P or Z to Z" << std::endl; // change this line each iteration
 
 	std::cin >> letter;
 	if (letter < 'A' || letter > 'Z') // not a capital letter
@@ -56,7 +56,31 @@ int main()
 		{
 			std::cout << "Australia, Antarctica, Aruba, Anguilla Leeward Islands, Antigua and Barbuda, American Samoa" << std::endl;
 		}
-	}	
+	}
+	switch (letter) // Start of Jay's code
+	{
+	case 'b':
+	case 'B':
+		switch (region)
+		{
+		case 1:
+			std::cout << "Belize, Bermuda, Bolivia, Brazil" << std::endl;
+			break;
+		case 2:
+			std::cout << "Belarus, Belgium, Bosnia and Herzegovina, Bulgaria" << std::endl;
+			break;
+		case 3:
+			std::cout << "Benin, Botswania, Burkina Faso, Burundi" << std::endl;
+			break;
+		case 4:
+			std::cout << "Bangladesh, Bhutan, Brunei Darussalam" << std::endl;
+			break;
+		case 5:
+			std::cout << "Bahamas, Bahrain Arabian Peninsula, Barbados" << std::endl;
+			break;
+		}
+		break;
+	} // End of Jay's code
 	if (letter == 'l' || letter == 'L') //Written by Conor
 	{
 		if (region == 1)
@@ -103,32 +127,6 @@ int main()
 			std::cout << "Marshall islands, Micronesia" << std::endl;
 		}
 	}
-
-	switch (letter) // Start of Jay's code
-	{
-		case 'b':
-		case 'B':
-			switch (region)
-			{
-				case 1:
-					std::cout << "Belize, Bermuda, Bolivia, Brazil" << std::endl;
-					break;
-				case 2:
-					std::cout << "Belarus, Belgium, Bosnia and Herzegovina, Bulgaria" << std::endl;
-					break;
-				case 3:
-					std::cout << "Benin, Botswania, Burkina Faso, Burundi" << std::endl;
-					break;
-				case 4:
-					std::cout << "Bangladesh, Bhutan, Brunei Darussalam" << std::endl;
-					break;
-				case 5:
-					std::cout << "Bahamas, Bahrain Arabian Peninsula, Barbados" << std::endl;
-					break;
-			}
-			break;
-	} // End of Jay's code
-
 	if (letter == 'n' || letter == 'N') //Written by Conor
 	{
 		if (region == 1)
@@ -175,7 +173,7 @@ int main()
 			std::cout << "There are no Os in the rest of the World" << std::endl;
 		} 
 	}
-	if (letter == 'p' || letter == 'P')
+	if (letter == 'p' || letter == 'P') //Written by Conor
 	{
 		if (region == 1)
 		{
