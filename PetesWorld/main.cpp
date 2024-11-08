7887a01177625af1ab5d19989a9adbbe0db45df2
Peter Lowe <lowep@itcarlow.ie>
2024-11-08 09:52:16 +0000

    Conors stuff

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -22,7 +22,7 @@ int main()
 		std::cout << "You need to select a number between 1 and 5" << std::endl;
 		return 0;
 	}
-	std::cout << "Pick a letter from A to A or Z to Z" << std::endl; // change this line each iteration
+	std::cout << "Pick a letter from A to A or N to N or Z to Z" << std::endl; // change this line each iteration
 	std::cin >> letter;
 	if (letter < 'A' || letter > 'Z') // not a capital letter
 	{ 
@@ -55,6 +55,29 @@ int main()
 			std::cout << "Australia, Antarctica, Aruba, Anguilla Leeward Islands, Antigua and Barbuda, American Samoa" << std::endl;
 		}
 	}	
+	if (letter == 'n' || letter == 'N') //Written by Conor
+	{
+		if (region == 1)
+		{
+			std::cout << "Nicaragua, Netherlands Antilles" << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "Norway, North Macedonia, Netherlands" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "Namibia, Niger, Nigeria" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "Nepal" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "Nauru, New Caledonia Melanesia, Niuen, North Marian islands" << std::endl;
+		}
+	}
 	if (letter == 'z' || letter == 'Z') // written by Pete
 	{
 		if (region == 1)
